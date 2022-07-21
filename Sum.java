a601d6d69fcf38bfbe30a2ce3868302d96b2a41d
SAGAR <sagarbarve65@gmail.com>
2022-07-21 16:49:49 +0530

    initial commit

---
+++ Sum.java
@@ -0,0 +1,13 @@
+package com.calc;
+
+public class Sum {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int a=5;
+		int b=10;
+		System.out.println(a+b);
+	}
+
+}
